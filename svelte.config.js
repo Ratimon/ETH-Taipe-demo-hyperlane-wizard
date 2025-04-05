a2b1ffeab307ca1be8e2e223b0b02f55dc7ecb98
Rati Montreewat <ratixoxo@gmail.com>
2025-04-05 08:16:50 +0800

    Feat (./) Using postcss and first theme config

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,16 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [
+		vitePreprocess(),
+		sveltePreprocess({
+			// postcss make use of tailwind
+			// we ensure it get processed, see postcss.config.cjs
+			postcss: true,
+		}),
+	],
 	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.svx']
 };
